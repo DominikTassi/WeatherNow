d72af39b85b0a241545315b92342947fbd88fdea
Tassi Dominik <doma_tassi@hotmail.com>
2017-11-30 19:35:23 +0100

    Started javascript

---
+++ web/src/main/webapp/js/sendweather.js
@@ -0,0 +1,47 @@
+function validate() {
+    var username = document.getElementById("username");
+    var town = document.getElementById("town");
+    var category = document.getElementById("category");
+    var temperature = document.getElementById("temperature");
+    if (username.value == "") {
+        username.style.backgroundColor = "red";
+        document.getElementById("username").placeholder = "Add your username";
+        username.style.color = "snow";
+        return false;
+    }
+    else{
+        username.style.backgroundColor = "green";
+        username.style.color = "snow";
+    }
+    if (town.value == "") {
+        town.style.backgroundColor = "red";
+        document.getElementById("town").placeholder = "Add the perception's town";
+        town.style.color = "snow";
+        return false;
+    }
+    else{
+        town.style.backgroundColor = "green";
+        town.style.color = "snow";
+    }
+    if (category.value == "") {
+        category.style.backgroundColor = "red";
+        document.getElementById("category").placeholder = "Category is needed";
+        category.style.color = "snow";
+        return false;
+    }
+    else{
+        category.style.backgroundColor = "green";
+        category.style.color = "snow";
+    }
+    if (temperature.value == "") {
+        temperature.style.backgroundColor = "red";
+        document.getElementById("temperature").placeholder = "Temperature is needed";
+        category.style.color = "snow";
+        return false;
+    }
+    else{
+        temperature.style.backgroundColor = "green";
+        temperature.style.color = "snow";
+    }
+    
+}
